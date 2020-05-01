bbd972c031148092faee2559af73323110cfb114
Jérémy Talabard <jeremy.talabard@public-areva.com>
2020-05-01 16:18:07 +0200

    Ajout d'un terrain + script ouverture porte

---
+++ Assets/Script/OpenDoor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private Animator _animator;
+
+    private bool _isOpened;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.tag == "Player")
+        {
+            if (!_isOpened)
+            {
+                _animator.Play("door_3_open");
+                _isOpened = true;
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider hit)
+    {
+        if (hit.gameObject.tag == "Player" && _isOpened)
+        {
+            _animator.Play("door_3_close");
+            _isOpened = false;
+        }
+    }
+}
